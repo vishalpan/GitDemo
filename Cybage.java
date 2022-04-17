59eb93d054421ee14599a717e64a2d3dd4980330
vishalpan <ervishalpandey@gmail.com>
2022-04-17 19:48:55 +0530

    First Commit to demo git

---
+++ Cybage.java
@@ -0,0 +1,14 @@
+class Cybage
+{
+	void Department()
+	{
+	System.out.println("QA: Best Department in IT Industry");
+	}
+	
+	public static void main(String[] args)
+	{
+		Cybage QA = new Cybage();
+		QA.Department();
+	}
+	
+}
---
+++ Planet.java
@@ -0,0 +1,14 @@
+class Planet
+{
+	void revolve()
+	{
+	System.out.println(" Planet Revolve");
+	}
+	
+	 public static void main(String[] args)
+	{
+	Planet earth = new Planet()
+	earth.revolve();
+	}
+	
+}
